04ea9edb450e4094c3e58f30789959e1b8c4e8a9
Eduardo Vianna <eduardovianna01@gmail.com>
2025-10-08 07:10:50 -0300

    fix: ajuste no commit anterior

--- src/entity/autor.ts
+++ src/entity/autor.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity()
-export class Livro {
+export class Autor {
   @PrimaryGeneratedColumn()
   id?: BigInt;
   @Column()
--- src/entity/leitor.ts
+++ src/entity/leitor.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity()
-export class Livro {
+export class Leitor {
   @PrimaryGeneratedColumn()
   id?: BigInt;
   @Column({nullable: false})
