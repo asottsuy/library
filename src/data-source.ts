2b98676e104e5b37b802cda2f16e6da621290550
Felipe Tatsuya Aso <asotatsuya0304@gmail.com>
2025-10-14 19:40:04 -0300

    fix: data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,26 @@
 import { DataSource } from "typeorm";
+import "dotenv/config"
+
+//local
+// export const myDataSource = new DataSource({
+//   type: "postgres",
+//   host: "localhost",
+//   port: 5432,
+//   username: "postgres",
+//   password: "password",
+//   database: "library-db",
+//   entities: ["src/entity/*.ts"],
+//   logging: true,
+//   dropSchema: false, //adicionar se quiser limpar o banco
+//   synchronize: true,
+//   subscribers: [],
+//   migrations: [],
+// });
+
+//prod
 export const myDataSource = new DataSource({
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "password",
-  database: "library-db",
-  entities: ["src/entity/*.ts"],
-  logging: true,
-  dropSchema: false, //adicionar se quiser limpar o banco
-  synchronize: true,
-  subscribers: [],
-  migrations: [],
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+    entities: ["src/entity/*.js"],
 });
