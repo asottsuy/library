2695df943efa666c24b40cb6df5b04ddffbc5f29
Felipe Tatsuya Aso <asotatsuya0304@gmail.com>
2025-11-28 18:48:41 -0300

    fix: ajuste do CORS

--- src/app.ts
+++ src/app.ts
@@ -35,7 +35,11 @@ myDataSource
     const app = express();
     const port = 3000;
     app.use(express.json());
-    app.use(cors()); 
+    app.use(cors({
+        origin: '*', 
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }));
     //Autor
     const autorRepository = myDataSource.getRepository(Autor);
     const autorService = new AutorService(autorRepository);
